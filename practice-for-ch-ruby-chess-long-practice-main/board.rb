a5e3edd05418bc4307e25b1394da6ae60533d925
jliangrpb <jeremy.liang612@gmail.com>
2023-03-28 16:16:03 -0400

    Almost done w board

---
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -0,0 +1,25 @@
+require_relative 'piece.rb'
+
+class Board 
+    def initialize
+        @rows = Array.new(8) {Array.new(8)}
+    end
+
+    def rows
+        @rows
+    end
+
+    def [](pos)
+        row,col = pos 
+        @rows[row][col]
+    end
+
+    def []=(pos, value)
+        row,col = pos 
+        @rows[row][col] = value
+    end
+
+    def move_piece(start_pos, end_pos)
+
+    end
+end
