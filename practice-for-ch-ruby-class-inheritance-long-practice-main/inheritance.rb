060f7f1512d019e87967077062faa017d92243bc
Arvid Hossain <arvidh97@gmail.com>
2023-03-28 15:09:29 -0400

    Added the employee manager connect

--- practice-for-ch-ruby-class-inheritance-long-practice-main/inheritance.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/inheritance.rb
@@ -1,29 +1,50 @@
 class Employee 
-    def initialize(name, title, salary)
+    attr_reader :boss
+    def initialize(name, title, salary, boss=nil)
         @name = name 
         @title = title 
         @salary = salary
-        @boss = nil 
+        @boss = boss 
+        # self.boss.employees << self if @boss != nil
     end
 
-    def boss=(new_boss)
-        @boss = new_boss if @boss.nil?
-    end
+    # def boss=(new_boss)
+    #     @boss = new_boss if @boss.nil?
+    # end
 
     def bonus(multiplier)
         @salary * multiplier
     end
 end
 
-class Manager < Employee 
-    def initialize
+class Manager < Employee
+    
+    def initialize(name, title, salary, boss=nil)
+        super
         @employees = []
+
     end
+
+    # def employees
+    #     @employees
+    # end
 
     def add_employee(new_employee)
         if new_employee.boss == self
             @employees << new_employee
         end
     end
+
+    def bonus(multipilier)
+        
+    end
+
 end
 
+a = Manager.new("frank", "big_boss", 1000000)
+b = Employee.new("bill", "little_man", 20000, a)
+a.add_employee(b)
+p a 
+
+puts " ___________________________"
+p b 
